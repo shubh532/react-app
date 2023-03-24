c4bca18c4257d30950c1ae81c66e0ae79a4121db
shubh532 <shubhammamahulkar2000>
2023-03-24 16:03:10 +0530

    ExpenseItem FIle Added

---
+++ src/My Components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <h6>Monthly Expense</h6>
+            <ol>
+                <li>Food- Rs:20</li>
+                <li>Petrol- Rs:108</li>
+                <li>Cloths- Rs:1500</li>
+                <li>Movies- Rs:20</li>
+            </ol>
+        </div>
+    )
+}
+export default ExpenseItem;
