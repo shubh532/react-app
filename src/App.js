c43eaa4a72be209c9c799fd805076378096b7933
shubh532 <shubhammamahulkar2000>
2023-03-24 20:35:15 +0530

    ExpenseLocation Added and Some CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './My Components/Expense.css'
 import ExpenseItem from './My Components/ExpenseItem'
 function App() {
   return (
--- src/My Components/ExpenseItem.js
+++ src/My Components/ExpenseItem.js
@@ -1,14 +1,34 @@
-function ExpenseItem(){
-    return(
-        <div>
-            <h6>Monthly Expense</h6>
-            <ol>
-                <li>Food- Rs:20</li>
-                <li>Petrol- Rs:108</li>
-                <li>Cloths- Rs:1500</li>
-                <li>Movies- Rs:20</li>
-            </ol>
-        </div>
-    )
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 3, 24);
+  const expenseTitle = "Food";
+  const expenseAmt = 250;
+  const expenseLocation = "Amravati";
+  return (
+    <div className="container">
+      <div className="heading-container">
+        <h6>Monthly Expense</h6>
+      </div>
+      <div className="item-container">
+        <ol>
+          <li>
+            {expenseDate.toISOString()} - {expenseTitle} - {expenseAmt}-
+            {expenseLocation}
+          </li>
+          <li>
+            {expenseDate.toISOString()} - {expenseTitle} - {expenseAmt}-
+            {expenseLocation}
+          </li>
+          <li>
+            {expenseDate.toISOString()} - {expenseTitle} - {expenseAmt}-
+            {expenseLocation}
+          </li>
+          <li>
+            {expenseDate.toISOString()} - {expenseTitle} - {expenseAmt}-
+            {expenseLocation}
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
