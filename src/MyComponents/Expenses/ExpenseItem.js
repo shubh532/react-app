ccd9877fcefc9b7ba366c83c0d1732731fee91a2
shubh532 <shubhammamahulkar2000>
2023-03-28 23:18:38 +0530

    ADD btn with useState Function

--- src/MyComponents/Expenses/ExpenseItem.js
+++ src/MyComponents/Expenses/ExpenseItem.js
@@ -1,16 +1,18 @@
+import React,{useState} from "react";
 import Date from "./Date"
 import "./Expense.css";
 function ExpenseItem(props) {
+  const[val,setVal]=useState(props.amount)
   const fun=()=>{
-    alert("BUTTON IS CLICKED")
+    setVal("100$")
   }
   return (
       <div className="expenseIem-container">
         <Date date={props.date}></Date>
         <div className="expense">{props.title}</div>
         <div className="location">{props.location}</div>
-        <div className="amount">{props.amount}</div>
-        <button onClick={fun} className="button">Delete</button>
+        <div className="amount">{val}</div>
+        <button onClick={fun} className="button">Add</button>
     </div>
   );
 }
