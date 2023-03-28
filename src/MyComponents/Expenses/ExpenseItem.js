aba4d2af3dd2b74465ea7bf0feb1e95315deaab2
shubh532 <shubhammamahulkar2000>
2023-03-28 14:19:48 +0530

    delete button added with function

--- src/MyComponents/Expenses/ExpenseItem.js
+++ src/MyComponents/Expenses/ExpenseItem.js
@@ -1,12 +1,16 @@
 import Date from "./Date"
 import "./Expense.css";
 function ExpenseItem(props) {
+  const fun=()=>{
+    alert("BUTTON IS CLICKED")
+  }
   return (
       <div className="expenseIem-container">
         <Date date={props.date}></Date>
         <div className="expense">{props.title}</div>
         <div className="location">{props.location}</div>
         <div className="amount">{props.amount}</div>
+        <button onClick={fun} className="button">Delete</button>
     </div>
   );
 }
